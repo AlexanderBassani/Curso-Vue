ddf5e257600ffad712471ab1283292163e558c35
Alexander Bassani Amaral <alexanderba09@gmail.com>
2024-03-03 06:46:04 -0300

    comunicacao

---
+++ comunicacao/src/components/Usuario.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é: <strong>{{ nome }}</strong></p>
+        <p>Idade é: <strong>{{ idade }}</strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <button @click="reiniciarIdade">Reiniciar Idade</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info 
+            :nome="nome" 
+            :idade="idade"
+            @eventoMudou="nome = $event" 
+            :reiniciarFn="reiniciarNome"/>
+            <!-- <app-usuario-info :nome="nome" @eventoMudou="nome = $event.novo + $event.antigo" :reiniciarFn="reiniciarNome"/> -->
+            <app-usuario-editar 
+            :idade="idade"
+            @idadeMudou="idade = $event"
+            :reiniciarIdadeFn="alterarIdade"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data(){
+        return {
+            nome: 'Pedro',
+            idade: 21
+        }
+    },
+    methods:{
+        alterarNome(){
+            this.nome = 'Ana'
+        },
+        reiniciarNome(){
+            this.nome = 'Pedro'
+        },
+        reiniciarIdade(){
+            this.idade = 21
+        },
+        alterarIdade(){
+            this.idade = 34
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome Usuário: <strong>{{ nomeAlternativo }}</strong></p>
+        <p>Nome Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Nome Usuário: <strong>{{ nome }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="reinicarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome (callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // props:['nome'],
+    props:{
+        // nome: String,
+        // nome: [String, Array],
+        nome: {
+            type: String,
+            default: 'Anônimo',
+            // default: function() {
+            //     return Array(10).fill(0).join('')
+            // },
+            // required: true
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    data() {
+        return {
+            nomeAlternativo: this.nome
+        }
+    },
+    methods:{
+        inverterNome(){
+            return this.nome.split('').reverse().join('')
+        },
+        reinicarNome(){
+            // this.nome = 'Pedro'
+            // this.$emit('eventoMudou', this.nome)
+
+            // const antigo = this.nome
+            const novo = "Pedro"
+            this.$emit('eventoMudou', novo)
+            // this.$emit('eventoMudou', {
+            //     novo: this.nome, || novo
+            //     antigo
+            // })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
